6470431e4434d103d8b773f12cd7d52320addd0f
pixlekind <hello.pixlekind@gmail.com>
2025-10-17 20:08:50 +0100

    update site

--- theme.js
+++
@@ -1,2 +0,0 @@
-const btn=document.getElementById("modeToggle");
-if(btn){btn.addEventListener("click",()=>{document.body.classList.toggle("light");btn.textContent=document.body.classList.contains("light")?"🌞":"🌙";});}
